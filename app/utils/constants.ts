f4dfee67447fa573d1716d6103273040bbdab0e7
LukasTr1980 <lukas.troebinger@gmail.com>
2024-10-09 18:05:47 +0200

    minor changes

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,4 +1,4 @@
-export const MAX_FILE_SIZE = 15 * 1024 * 1024; // 5 MB
+export const MAX_FILE_SIZE = 20 * 1024 * 1024;
 
 export const ALLOWED_MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/webp', 'application/pdf'];
 
